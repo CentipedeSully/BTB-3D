368914cf16118bc5d8b30f8f703ec80faef4134b
William Sullivan-Smith <sullivansmith057@gmail.com>
2026-04-12 13:00:18 -0500

    Implemented merchants populating their containers. Now you can buy stuff from merchants if you have enough currency. You can also reroll their stock.

--- Assets/dts_Inventory/Demo Assets/Scripts/Controllers/MerchantController.cs
+++ Assets/dts_Inventory/Demo Assets/Scripts/Controllers/MerchantController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 
@@ -11,16 +13,20 @@ namespace dtsInventory
         [SerializeField] private GameObject _merchantInvWindowPrefab;
         private Canvas _canvas;
         private InvWindow _merchantInvWindow;
+        
 
         [Header("Stock Settings")]
         [SerializeField] private float _interactRadius = 1.5f;
         [SerializeField] private List<LootRoll> _stockChancesList;
-        [SerializeField] private bool _rerollStock;
+        [SerializeField] private bool _showRestockResults;
+        private IEnumerator _wareRegenerator;
+        
 
 
         [Header("Debug")]
         [SerializeField] private bool _isDebugActive = false;
         [SerializeField] private bool _cmdToggleUi = false;
+        [SerializeField] private bool _cmdRerollStock;
 
 
         //monobehaviours
@@ -48,6 +54,12 @@ namespace dtsInventory
 
 
         //internals
+        private IEnumerator RegenStockAtEndOfFrame()
+        {
+            yield return new WaitForEndOfFrame();
+            RepopulateMerhcantContainer();
+            _wareRegenerator = null;
+        }
         private void CreateNewMerchantUi()
         {
             if (_merchantInvWindow == null)
@@ -64,7 +76,48 @@ namespace dtsInventory
             }
         }
 
+        private void RepopulateMerhcantContainer()
+        {
+            if (_merchantInvWindow == null)
+            {
+                Debug.LogWarning("Attempted to regenerate merchant wares when merchant isn't initialized yet. Open the merchant to generate its wares the first time");
+                return;
+            }
+            //first, remove all items from the merchant's container
+            InvGrid merchantGrid = _merchantInvWindow.GetItemGrid();
+            int removalIterations = 0;
+            while (merchantGrid.GetAllStacks().Count > 0 && removalIterations <=1000) //this shouldn't take 1000 iterations. This demo doesn't contain 1000 different itemDatas
+            {
+                ItemData chosenItem = merchantGrid.GetAllStacks().First().Value;
+                merchantGrid.RemoveItem(chosenItem, merchantGrid.CountItem(chosenItem));
+                removalIterations++;
+            }
 
+            if (removalIterations > 1000 && merchantGrid.GetAllStacks().Count > 0)
+            {
+                Debug.LogWarning($"Failsafe detected: averted an infinite while loop while attempting to remove all items from a merchant's container {merchantGrid.name}. Aborting operation.");
+                return;
+            }
+
+            //now reroll the merchant's loot
+            List<bool> lootRollResults = ContainerController.RollForLoot(_stockChancesList, _showRestockResults);
+
+            //add all successfully-rolled items to the container
+            for (int i = 0; i < _stockChancesList.Count; i++)
+            {
+
+                if (lootRollResults[i] == true)
+                {
+                    int amountToAdd = UnityEngine.Random.Range(_stockChancesList[i].minAmount, _stockChancesList[i].maxAmount + 1);
+
+                    if (_showRestockResults)
+                        Debug.Log($"Rolled Amount to add: {amountToAdd}");
+
+                    _merchantInvWindow.GetItemGrid().AddItem(_stockChancesList[i].itemdata, amountToAdd);
+                }
+
+            }
+        }
 
 
         //externals
@@ -76,7 +129,7 @@ namespace dtsInventory
                 CreateNewMerchantUi();
 
                 //Don't forget to Restock
-                //...
+                RegenerateWares();
             }
 
             if (!_merchantInvWindow.IsWindowOpen())
@@ -116,6 +169,15 @@ namespace dtsInventory
             return _interactRadius;
         }
 
+        public void RegenerateWares()
+        {
+            //only attempt to regen if we aren't already regen'ing
+            if (_wareRegenerator == null)
+            {
+                _wareRegenerator = RegenStockAtEndOfFrame();
+                StartCoroutine( _wareRegenerator );
+            }
+        }
 
 
         //Debug
@@ -131,6 +193,11 @@ namespace dtsInventory
                     CloseMerchantUi();
                 else OpenMerchantUi();
                 
+            }
+            if (_cmdRerollStock)
+            {
+                _cmdRerollStock = false;
+                RegenerateWares();
             }
         }
 
